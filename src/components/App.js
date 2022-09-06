f0711bc26d57267f407f77f9ee4e95ca4344b0a4
julyusmanurung <julyusmanurung@gmail.com>
2022-09-06 22:46:04 +0700

    add search features

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
 class App extends Component {
-  state = { artistQuery: '' };
+  state = { artistQuery: '', artist: null, tracks: []};
   updateArtistQuery = event => {
-    console.log('event.target.value', event.target.value);
     this.setState({ artistQuery: event.target.value });
   }
   handleKeyPress = event => {
@@ -12,7 +13,19 @@ class App extends Component {
     }
   }
   searchArtist = () => {
-    console.log('this.state', this.state);
+    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+    .then(response => response.json())
+    .then(json => {
+      if (json.artist.total > 0) {
+        const artist = json.artist.item[0];
+        this.setState({ artist })
+        fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+        .then(response => response.json())
+        .then(json => this.setState({ tracks: json.tracks }))
+        .catch(error => alert(error.message));
+      }
+    })
+    .catch(error => alert(error.message));
   }
   render () {
     return (
